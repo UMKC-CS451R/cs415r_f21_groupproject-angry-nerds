28bc2b124aaf8fb959b77107ccaaa3af04170233
Richard <rschneider1st@gmail.com>
2021-10-16 14:12:39 -0500

    added token authorization

---
+++ Controllers/API/AuthorizationController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using src.Models;
+using src.Services;
+
+namespace src.Controllers
+{
+    [ApiController]
+    public class AuthorizationController : ControllerBase
+    {
+        private IUserService _userService;
+
+        public AuthorizationController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost()]
+        [Route("api/getToken")]
+        public IActionResult Authenticate(AuthenticateRequest model)
+        {
+            var response = _userService.Authenticate(model);
+
+            if (response == null)
+                return BadRequest(new { message = "Username or password is incorrect" });
+
+            return Ok(response);
+        }
+
+        [Authorize]
+        [HttpGet]
+        [Route("api/getUsers")]
+        public IActionResult GetAll()
+        {
+            var users = _userService.GetAll();
+            return Ok(users);
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using src.Helpers;
+using src.Services;
 
 namespace CS451RWebApp
 {
@@ -23,7 +25,14 @@ namespace CS451RWebApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
             services.AddControllersWithViews();
+
+            // configure strongly typed settings object
+            services.Configure<AppSettings>(Configuration.GetSection("AppSettings"));
+
+            // configure DI for application services
+            services.AddScoped<IUserService, UserService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +54,15 @@ namespace CS451RWebApp
             app.UseRouting();
 
             app.UseAuthorization();
+
+            // global cors policy
+            app.UseCors(x => x
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+
+            // custom jwt auth middleware
+            app.UseMiddleware<JwtMiddleware>();
 
             app.UseEndpoints(endpoints =>
             {
