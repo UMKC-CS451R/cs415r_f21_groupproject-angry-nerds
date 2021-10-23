2439c8f84a1abe8e19cc9362735a8f369605a4c5
AhmedBoukhousse <47191893+AhmedBoukhousse@users.noreply.github.com>
2021-10-23 13:18:16 -0500

    init

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,54 @@
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Commerce Bank',
+    headline: 'Unlimited Transactions with zero fees',
+    description:
+      'Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees',
+    buttonLabel: 'Get Started',
+    imgStart: false,
+    img: require('../../images/unlimited.png'),
+    alt: 'Car',
+    dark: true,
+    primary: true,
+    darkText: false
+  };
+  
+  export const homeObjTwo = {
+    id: 'discover',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Unlimited Access',
+    headline: 'Login to your account any where from any device',
+    description:
+      'We have you covered no matter where you are located. All you need is an internet connection and a phone or computer.',
+    buttonLabel: 'Learn More',
+    imgStart: true,
+    img: require('../../images/svg-2.svg'),
+    alt: 'Piggybank',
+    dark: false,
+    primary: false,
+    darkText: true
+  };
+  
+  export const homeObjThree = {
+    id: 'signup',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Join our Team',
+    headline: 'Creating an account is extremely easy',
+    description:
+      "Add some cool statments here",
+    buttonLabel: 'Start Now',
+    imgStart: false,
+    img: require('../../images/svg-3.svg'),
+    alt: 'Papers',
+    dark: false,
+    primary: false,
+    darkText: true
+  };
+  
