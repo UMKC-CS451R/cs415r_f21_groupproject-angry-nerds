c11944ecbe2c18ce214bbfeff5083cbe9b895609
JoseFonseca23 <colar.m.a@gmail.com>
2021-10-25 20:31:38 -0500

    Changed logo
    Added a white logo and changed it in Navbar/idex.js

--- Frontend/src/components/Navbar/index.js
+++ Frontend/src/components/Navbar/index.js
@@ -1,6 +1,6 @@
 
 import React, { useState, useEffect } from 'react';
-import logo from './log.png';
+import logo from './logo.png';
 import { FaBars } from 'react-icons/fa';
 import { IconContext } from 'react-icons/lib';
 import { animateScroll as scroll } from 'react-scroll';
